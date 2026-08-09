8c2330cb6d5a06fd0c88dc51dc210f2ef4a67d3e
mearin0424-tech <mearin0424@gmail.com>
2026-08-09 14:34:55 +0900

    0809

--- resources/views/shops/home/index.blade.php
+++ resources/views/shops/home/index.blade.php
@@ -4,7 +4,7 @@
 @section('body-class', 'no-scroll page-home')
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}?v=20260809-profile-compact">
+<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}?v=20260809-profile-relayout">
 {{-- Perf / tier-chip / cssMode overrides live in a separate file; load AFTER home.css. --}}
 <link rel="stylesheet" href="{{ asset('assets/css/home-perf.css') }}?v=20260809-avail-tag">
 @endpush
